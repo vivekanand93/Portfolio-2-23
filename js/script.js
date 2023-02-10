3b6abd046facfcf9677d18960b096ec6f9e39223
VIVEK ANAND <78248575+vivekanand93@users.noreply.github.com>
2023-02-10 12:43:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+function myFunction() {
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toLocaleString();
+}
